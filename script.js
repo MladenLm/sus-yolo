e2ffaf684148203a8b1598336ea6426859e0ebad
MladenLm <mladenlamesevic@gmail.com>
2023-02-25 15:54:49 +0100

    Added 404 not found page

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle(x) {
+    x.classList.toggle("change");
+    // document.getElementById("myNav").style.width = "100%";
+  }
+
+// function closeNav() {
+//     document.getElementById("myNav").style.width = "0%"
+// }
